ab31fa177e6cbafd8f33d0fd5bc7f8ec7944bc5f
sorabh kumar <sorabhkumardeveloper@gmail.com>
2022-10-25 17:58:29 +0530

    fix: Fixed MainFragment.kt code issues

--- app/src/main/java/com/sorabh/grabfood/ui/activities/MainFragment.kt
+++ app/src/main/java/com/sorabh/grabfood/ui/activities/MainFragment.kt
@@ -5,29 +5,28 @@ import android.content.DialogInterface
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textview.MaterialTextView
 import com.sorabh.grabfood.R
 import com.sorabh.grabfood.databinding.MainFragmentBinding
 import com.sorabh.grabfood.ui.fragments.cart.CartFragment
 import com.sorabh.grabfood.ui.fragments.favorites.FavoriteRestaurantsFragment
 import com.sorabh.grabfood.ui.fragments.history.OderHistoryFragment
+import com.sorabh.grabfood.ui.fragments.home.BaseFragment
 import com.sorabh.grabfood.ui.fragments.home.HomeFragment
 import com.sorabh.grabfood.ui.fragments.profile.MyProfileFragment
 import com.sorabh.grabfood.ui.fragments.qna.QNAFragment
-import com.sorabh.grabfood.ui.fragments.home.BaseFragment
+import com.sorabh.grabfood.util.Keys
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -41,13 +40,21 @@ class MainFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-       binding = MainFragmentBinding.inflate(layoutInflater)
+        startupInitializer()
+        setupDrawerLayout()
+        setupHeaderLayout()
+        changeFragmentAndTitle(HomeFragment(), getString(R.string.restaurants_list))
+        setupNavigationItemSelector()
+        return binding.root
+    }
+
+    private fun startupInitializer() {
+        binding = MainFragmentBinding.inflate(layoutInflater)
         navController = findNavController()
-
-        //initializing toolBar
         (activity as AppCompatActivity).setSupportActionBar(binding.toolBar)
+    }
 
-        //for easy traction of drawer navigation
+    private fun setupDrawerLayout() {
         val actionBarDrawerToggle = ActionBarDrawerToggle(
             requireActivity(),
             binding.drawerLayout,
@@ -57,71 +64,52 @@ class MainFragment : BaseFragment() {
         )
         binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+    }
 
-        //default fragment that will be show to user
-        changeFragmentAndTitle(HomeFragment(), "Restaurant List", binding)
+    private fun setupHeaderLayout() {
+        sharedPreferences = requireContext().getSharedPreferences(Keys.LOGIN, MODE_PRIVATE)
 
-        //exacting data from sharedPreference
-        sharedPreferences = requireContext().getSharedPreferences("login", MODE_PRIVATE)
-        val name = sharedPreferences.getString("name", "unknown")
-        val email = sharedPreferences.getString("email", "unknown")
-
-        //exacting  navigation's header layout view
         headerView = binding.navigationView.getHeaderView(0)
-        headerView.findViewById<MaterialTextView>(R.id.txt_main_username).text = name
-        headerView.findViewById<MaterialTextView>(R.id.txt_main_email).text = email
+        headerView.findViewById<MaterialTextView>(R.id.txt_main_username).text =
+            sharedPreferences.getString(Keys.NAME, getString(R.string.unknown))
 
+        headerView.findViewById<MaterialTextView>(R.id.txt_main_email).text =
+            sharedPreferences.getString(Keys.EMAIL, getString(R.string.unknown))
+    }
 
-        //working with navigation menu components
+    private fun setupNavigationItemSelector() {
         binding.navigationView.setNavigationItemSelectedListener {
-            //changing fragment with there respective menu id
+            it.isChecked = true
             when (it.itemId) {
                 R.id.drawer_menu_home -> {
-                    changeFragmentAndTitle(
-                        HomeFragment(),
-                        "Restaurant List",
-                        binding
-                    )
+                    changeFragmentAndTitle(HomeFragment(), getString(R.string.restaurants_list))
                     binding.searchView.isVisible = true
-                    it.isChecked = true
 
                 }
                 R.id.drawer_menu_profile -> {
-                    changeFragmentAndTitle(MyProfileFragment(), "My Profile", binding)
+                    changeFragmentAndTitle(MyProfileFragment(), getString(R.string.my_profile))
                     binding.searchView.isVisible = false
-                    it.isChecked = true
-
                 }
                 R.id.drawer_menu_cart -> {
-                    changeFragmentAndTitle(CartFragment(), "My cart", binding)
+                    changeFragmentAndTitle(CartFragment(), getString(R.string.my_cart))
                     binding.searchView.isVisible = false
-                    it.isChecked = true
                 }
                 R.id.drawer_menu_history -> {
-                    changeFragmentAndTitle(OderHistoryFragment(), "Oder History", binding)
+                    changeFragmentAndTitle(OderHistoryFragment(), getString(R.string.oder_history))
                     binding.searchView.isVisible = false
-                    it.isChecked = true
-
                 }
                 R.id.drawer_menu_qna -> {
-                    changeFragmentAndTitle(QNAFragment(), "Frequently Asked Question", binding)
+                    changeFragmentAndTitle(QNAFragment(), getString(R.string.frequently_asked))
                     binding.searchView.isVisible = false
-                    it.isChecked = true
 
                 }
                 R.id.drawer_menu_favorite -> {
                     changeFragmentAndTitle(
-                        FavoriteRestaurantsFragment(),
-                        "My Favorite Restaurants",
-                        binding
+                        FavoriteRestaurantsFragment(), getString(R.string.my_favorite_restaurants)
                     )
                     binding.searchView.isVisible = false
-                    it.isChecked = true
-
 
                 }
-
-                // logout from the app and redirect user from login activity
                 R.id.drawer_menu_logout -> {
                     confirmLogOutDialog()
                     it.isChecked = true
@@ -131,15 +119,12 @@ class MainFragment : BaseFragment() {
             }
             return@setNavigationItemSelectedListener true
         }
-        return binding.root
     }
 
     //for fragment transaction between fragment
     private fun changeFragmentAndTitle(
         fragment: Fragment,
-        title: String,
-        binding: MainFragmentBinding
-    ) {
+        title: String) {
         (activity as AppCompatActivity).supportActionBar?.title = title
         childFragmentManager.beginTransaction()
             .setCustomAnimations(
@@ -154,37 +139,15 @@ class MainFragment : BaseFragment() {
         binding.drawerLayout.closeDrawer(GravityCompat.START)
     }
 
-    //for toolbar setup
-//    private fun setUpActionbar(toolbar: Toolbar?) {
-//        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-//        (activity as AppCompatActivity).supportActionBar?.setHomeButtonEnabled(true)
-//        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        (activity as AppCompatActivity).supportActionBar?.title = "Home"
-//    }
-
-    //to listen event on  hamburger icon click
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == android.R.id.home) {
-            binding.drawerLayout.openDrawer(GravityCompat.START)
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    //confirm dialog
     private fun confirmLogOutDialog() {
-        val alertDialog = AlertDialog.Builder(requireContext()).create()
-        alertDialog.setTitle("Are You Want to Log Out!")
-        alertDialog.setMessage("After clicking Yes you will be log out from the App.")
+        val alertDialog = MaterialAlertDialogBuilder(requireContext()).create()
+        alertDialog.setTitle(getString(R.string.are_you_sure_to_log_out))
+        alertDialog.setMessage(getString(R.string.after_clicking_yes_you_will_be))
         alertDialog.setIcon(R.drawable.icon_grabfood)
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "LogOut") { _: DialogInterface, _: Int ->
-            val sharedPreferences = requireContext().getSharedPreferences("login", MODE_PRIVATE)
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.yes)) { _: DialogInterface, _: Int ->
+            val sharedPreferences = requireContext().getSharedPreferences(Keys.LOGIN, MODE_PRIVATE)
             sharedPreferences.edit().clear().apply()
-            Toast.makeText(
-                context,
-                "You Log Out Successfully from GrabFood",
-                Toast.LENGTH_SHORT
-            ).show()
+            showToast(getString(R.string.you_successfully_log_out_from_the_app))
             navController.navigate(
                 MainFragmentDirections.actionMainFragmentToLoginFragment()
             )
@@ -192,7 +155,7 @@ class MainFragment : BaseFragment() {
         }
         alertDialog.setButton(
             AlertDialog.BUTTON_POSITIVE,
-            "Keep LogIn"
+            getString(R.string.discard)
         ) { dialog: DialogInterface, _: Int ->
             dialog.dismiss()
         }
