3734935108006ef4bd636cc5071fa3ff574c5f18
sorabh kumar <sorabhkumardeveloper@gmail.com>
2022-10-28 23:11:17 +0530

    fix: fixed some ui and code issue in favorite_restaurants_fragment.xml

--- app/src/main/java/com/sorabh/grabfood/ui/fragments/favorites/FavoriteRestaurantsFragment.kt
+++ app/src/main/java/com/sorabh/grabfood/ui/fragments/favorites/FavoriteRestaurantsFragment.kt
@@ -1,85 +1,67 @@
 package com.sorabh.grabfood.ui.fragments.favorites
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.lifecycleScope
 import com.sorabh.grabfood.R
 import com.sorabh.grabfood.databinding.FavoriteRestaurantsFragmentBinding
 import com.sorabh.grabfood.domain.model.reataurants_home_response.Dish
 import com.sorabh.grabfood.domain.repository.LocalDBRepository
 import com.sorabh.grabfood.ui.adapter.RestaurantHomeAdapter
 import com.sorabh.grabfood.ui.adapter.RestaurantViewHolder
+import com.sorabh.grabfood.ui.fragments.home.BaseFragment
 import com.sorabh.grabfood.ui.fragments.restaurant_menu.RestaurantMenuFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class FavoriteRestaurantsFragment : Fragment(),
+class FavoriteRestaurantsFragment : BaseFragment(),
     RestaurantViewHolder.OnRestaurantsClicked,
     RestaurantViewHolder.OnFavoriteButtonClicked {
     private lateinit var binding:FavoriteRestaurantsFragmentBinding
-    private val job = SupervisorJob()
 
     @Inject
     lateinit var restaurantHomeAdapter: RestaurantHomeAdapter
     @Inject
     lateinit var localDBRepository: LocalDBRepository
 
+    private lateinit var favoriteRestaurantList: List<Dish>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FavoriteRestaurantsFragmentBinding.inflate(layoutInflater)
-        //Changing toolbar title
-        (activity as AppCompatActivity).supportActionBar?.title = "My Favorite Restaurants"
-
-
-        CoroutineScope(job + Dispatchers.IO).launch {
-            restaurantHomeAdapter.restaurantsClicked = this@FavoriteRestaurantsFragment
-            restaurantHomeAdapter.onFavoriteButtonClicked = this@FavoriteRestaurantsFragment
-
-            val lytManager = LinearLayoutManager(activity as Context)
-            lateinit var favoriteRestaurantList: List<Dish>
-            try {
-                favoriteRestaurantList = getFavoriteRestaurantList()!!
-            } catch (e: Exception) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(
-                        activity as Context,
-                        "Failed to connect Server",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-            withContext(Dispatchers.Main) {
-                with(binding.favoriteRestaurantRecyclerview) {
-                    adapter = restaurantHomeAdapter
-                    layoutManager = lytManager
-                    restaurantHomeAdapter.updateRestaurantsList(favoriteRestaurantList)
-                    binding.favoriteRestaurantProgressBar.visibility =
-                        ProgressBar.GONE
-                }
-            }
-        }
-
+        startupInitializer()
+        setupData()
         return binding.root
     }
 
-    private suspend fun getFavoriteRestaurantList(): List<Dish>? = coroutineScope {
-        val list = CoroutineScope(job + Dispatchers.IO).async {
-            return@async localDBRepository.getRestaurantList()
-        }
-        return@coroutineScope list.await()
+    private fun startupInitializer() {
+        binding = FavoriteRestaurantsFragmentBinding.inflate(layoutInflater)
+        (activity as AppCompatActivity).supportActionBar?.title =
+            getString(R.string.my_favorite_restaurants)
+        binding.favoriteRestaurantRecyclerview.adapter = restaurantHomeAdapter
+        restaurantHomeAdapter.restaurantsClicked = this
+        restaurantHomeAdapter.onFavoriteButtonClicked = this
+    }
 
+    private fun setupData() {
+        lifecycleScope.launch {
+            try {
+                favoriteRestaurantList = getFavoriteRestaurantList()!!
+                restaurantHomeAdapter.updateRestaurantsList(favoriteRestaurantList)
+                binding.favoriteRestaurantProgressBar.visibility = ProgressBar.GONE
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    showToast(getString(R.string.something_went_wrong))
+                }
+            }
+        }
     }
 
     override fun onRestaurantsClicked(dish: Dish) {
@@ -96,37 +78,31 @@ class FavoriteRestaurantsFragment : Fragment(),
     }
 
     override fun onFavoriteButtonClicked(dish: Dish) {
-        Log.d("exc", "executed")
-        CoroutineScope(job + Dispatchers.IO).launch {
-
+        lifecycleScope.launch {
             val restaurantData = localDBRepository.getRestaurant(dish.id)
-
             if (restaurantData != null) {
                 localDBRepository.deleteRestaurant(dish)
-                val list = localDBRepository.getRestaurantList()
+                val list = getFavoriteRestaurantList()
                 withContext(Dispatchers.Main) {
-                    Toast.makeText(
-                        context,
-                        "Deleted to Your Favorite Restaurant",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
+                    showToast(getString(R.string.deleted_to_your_favorite_restaurants))
                     restaurantHomeAdapter.updateRestaurantsList(list)
                 }
             } else {
                 localDBRepository.insertRestaurant(dish)
-                val list = localDBRepository.getRestaurantList()
+                val list = getFavoriteRestaurantList()
                 withContext(Dispatchers.Main) {
-                    Toast.makeText(
-                        context,
-                        "Added  to Your Favorite Restaurant",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
+                   showToast(getString(R.string.added_to_your_favorite_restaurants))
                     restaurantHomeAdapter.updateRestaurantsList(list)
 
                 }
             }
         }
+    }
+
+    private suspend fun getFavoriteRestaurantList(): List<Dish>? = coroutineScope {
+        val list = CoroutineScope(Dispatchers.IO).async {
+            return@async localDBRepository.getRestaurantList()
+        }
+        return@coroutineScope list.await()
     }
 }
