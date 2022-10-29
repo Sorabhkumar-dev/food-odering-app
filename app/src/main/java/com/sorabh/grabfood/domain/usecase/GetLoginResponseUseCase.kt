3ce9b1409ce40a34ad36810db10865efcbc86618
sorabh kumar <sorabhkumardeveloper@gmail.com>
2022-10-29 07:44:05 +0530

    chore:incremented dependency version

--- app/src/main/java/com/sorabh/grabfood/domain/usecase/GetLoginResponseUseCase.kt
+++ app/src/main/java/com/sorabh/grabfood/domain/usecase/GetLoginResponseUseCase.kt
@@ -7,5 +7,6 @@ import com.sorabh.grabfood.domain.repository.NetworkRepository
 import javax.inject.Inject
 
 class GetLoginResponseUseCase @Inject constructor(private val networkRepository: NetworkRepository):BaseUsaCase<LoginPostModel,LoginResponse>() {
-    override suspend fun getData(params: LoginPostModel?): Result<LoginResponse> = networkRepository.getLoginDetails(params!!.header,params!!.params)
+    override suspend fun getData(params: LoginPostModel?): Result<LoginResponse> = networkRepository.getLoginDetails(params!!.header,
+        params.params)
 }
