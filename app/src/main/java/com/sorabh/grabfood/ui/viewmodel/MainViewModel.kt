5d1b50b841336f458e5ffca0a745837b941545a3
sorabh kumar <sorabhkumardeveloper@gmail.com>
2022-11-11 01:10:19 +0530

    fix: fixed when i log-out from the app then i try to login into app but i unable to login (github issue)

--- app/src/main/java/com/sorabh/grabfood/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sorabh/grabfood/ui/viewmodel/MainViewModel.kt
@@ -1,12 +1,20 @@
 package com.sorabh.grabfood.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sorabh.grabfood.domain.datastore.PreferenceData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(preferenceData: PreferenceData):ViewModel() {
+class MainViewModel @Inject constructor(private val preferenceData: PreferenceData) : ViewModel() {
     val nameFlow = preferenceData.readNameFlow
     val emailFlow = preferenceData.readEmailFlow
+
+    fun clearDataStorePreference() {
+        viewModelScope.launch {
+            preferenceData.clearDatStore()
+        }
+    }
 }
