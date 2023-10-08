3d5800ba92ae1b5d68863ccb892a4e9f355faa58
Sorabh Kumar <sorabh.kumar@stylework.city>
2023-10-08 22:24:56 +0530

    chore:migrated all screen into Jetpack compose

--- app/build/tmp/kapt3/stubs/debug/com/sorabh/grabfood/ui/activities/OTPFragment.java
+++ app/build/tmp/kapt3/stubs/debug/com/sorabh/grabfood/ui/activities/OTPFragment.java
@@ -4,32 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.lifecycle.Lifecycle;
+import androidx.compose.ui.platform.ComposeView;
 import androidx.navigation.NavController;
-import com.google.gson.JsonObject;
-import com.sorabh.grabfood.R;
-import com.sorabh.grabfood.databinding.OtpFragmentBinding;
-import com.sorabh.grabfood.domain.model.post.OderPostModel;
-import com.sorabh.grabfood.domain.network_api.Result;
-import com.sorabh.grabfood.domain.repository.NetworkRepository;
 import com.sorabh.grabfood.ui.fragments.home.BaseFragment;
 import com.sorabh.grabfood.ui.viewmodel.OtpViewModel;
-import com.sorabh.grabfood.util.Constants;
-import com.sorabh.grabfood.util.Keys;
 import dagger.hilt.android.AndroidEntryPoint;
-import javax.inject.Inject;
 
 @dagger.hilt.android.AndroidEntryPoint
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000X\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0006\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J$\u0010\u0019\u001a\u00020\u001a2\u0006\u0010\u001b\u001a\u00020\u001c2\b\u0010\u001d\u001a\u0004\u0018\u00010\u001e2\b\u0010\u001f\u001a\u0004\u0018\u00010 H\u0016J\b\u0010!\u001a\u00020\"H\u0002J(\u0010#\u001a\u00020\"2\u0006\u0010$\u001a\u00020%2\u0006\u0010&\u001a\u00020%2\u0006\u0010\'\u001a\u00020%2\u0006\u0010(\u001a\u00020%H\u0002J\b\u0010)\u001a\u00020\"H\u0002J\u0010\u0010*\u001a\u00020\"2\u0006\u0010\u001b\u001a\u00020\u001cH\u0002R\u001b\u0010\u0003\u001a\u00020\u00048BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000b\u001a\u00020\fX\u0082.\u00a2\u0006\u0002\n\u0000R\u001e\u0010\r\u001a\u00020\u000e8\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000f\u0010\u0010\"\u0004\b\u0011\u0010\u0012R\u001b\u0010\u0013\u001a\u00020\u00148BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0017\u0010\u0018\u001a\u0004\b\u0015\u0010\u0016\u00a8\u0006+"}, d2 = {"Lcom/sorabh/grabfood/ui/activities/OTPFragment;", "Lcom/sorabh/grabfood/ui/fragments/home/BaseFragment;", "()V", "args", "Lcom/sorabh/grabfood/ui/activities/OTPFragmentArgs;", "getArgs", "()Lcom/sorabh/grabfood/ui/activities/OTPFragmentArgs;", "args$delegate", "Landroidx/navigation/NavArgsLazy;", "binding", "Lcom/sorabh/grabfood/databinding/OtpFragmentBinding;", "navController", "Landroidx/navigation/NavController;", "repository", "Lcom/sorabh/grabfood/domain/repository/NetworkRepository;", "getRepository", "()Lcom/sorabh/grabfood/domain/repository/NetworkRepository;", "setRepository", "(Lcom/sorabh/grabfood/domain/repository/NetworkRepository;)V", "viewModel", "Lcom/sorabh/grabfood/ui/viewmodel/OtpViewModel;", "getViewModel", "()Lcom/sorabh/grabfood/ui/viewmodel/OtpViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "setOnClickListener", "", "setupApiCall", "phone", "", "otp", "password", "confirmPassword", "setupObserver", "startupInitializer", "app_debug"})
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J$\u0010\u0011\u001a\u00020\u00122\u0006\u0010\u0013\u001a\u00020\u00142\b\u0010\u0015\u001a\u0004\u0018\u00010\u00162\b\u0010\u0017\u001a\u0004\u0018\u00010\u0018H\u0016R\u001b\u0010\u0003\u001a\u00020\u00048BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u000b\u001a\u00020\f8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u000f\u0010\u0010\u001a\u0004\b\r\u0010\u000e\u00a8\u0006\u0019"}, d2 = {"Lcom/sorabh/grabfood/ui/activities/OTPFragment;", "Lcom/sorabh/grabfood/ui/fragments/home/BaseFragment;", "()V", "args", "Lcom/sorabh/grabfood/ui/activities/OTPFragmentArgs;", "getArgs", "()Lcom/sorabh/grabfood/ui/activities/OTPFragmentArgs;", "args$delegate", "Landroidx/navigation/NavArgsLazy;", "navController", "Landroidx/navigation/NavController;", "viewModel", "Lcom/sorabh/grabfood/ui/viewmodel/OtpViewModel;", "getViewModel", "()Lcom/sorabh/grabfood/ui/viewmodel/OtpViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
 public final class OTPFragment extends com.sorabh.grabfood.ui.fragments.home.BaseFragment {
     @org.jetbrains.annotations.NotNull
     private final kotlin.Lazy viewModel$delegate = null;
     @org.jetbrains.annotations.NotNull
     private final androidx.navigation.NavArgsLazy args$delegate = null;
-    private com.sorabh.grabfood.databinding.OtpFragmentBinding binding;
     private androidx.navigation.NavController navController;
-    @javax.inject.Inject
-    public com.sorabh.grabfood.domain.repository.NetworkRepository repository;
     
     public OTPFragment() {
         super();
@@ -43,15 +31,6 @@ public final class OTPFragment extends com.sorabh.grabfood.ui.fragments.home.Bas
         return null;
     }
     
-    @org.jetbrains.annotations.NotNull
-    public final com.sorabh.grabfood.domain.repository.NetworkRepository getRepository() {
-        return null;
-    }
-    
-    public final void setRepository(@org.jetbrains.annotations.NotNull
-    com.sorabh.grabfood.domain.repository.NetworkRepository p0) {
-    }
-    
     @java.lang.Override
     @org.jetbrains.annotations.NotNull
     public android.view.View onCreateView(@org.jetbrains.annotations.NotNull
@@ -59,17 +38,5 @@ public final class OTPFragment extends com.sorabh.grabfood.ui.fragments.home.Bas
     android.view.ViewGroup container, @org.jetbrains.annotations.Nullable
     android.os.Bundle savedInstanceState) {
         return null;
-    }
-    
-    private final void startupInitializer(android.view.LayoutInflater inflater) {
-    }
-    
-    private final void setOnClickListener() {
-    }
-    
-    private final void setupApiCall(java.lang.String phone, java.lang.String otp, java.lang.String password, java.lang.String confirmPassword) {
-    }
-    
-    private final void setupObserver() {
     }
 }
--- app/build/tmp/kapt3/stubs/debug/com/sorabh/grabfood/ui/viewmodel/OtpViewModel.java
+++ app/build/tmp/kapt3/stubs/debug/com/sorabh/grabfood/ui/viewmodel/OtpViewModel.java
@@ -1,15 +1,17 @@
 package com.sorabh.grabfood.ui.viewmodel;
 
 import androidx.lifecycle.ViewModel;
+import com.google.gson.JsonObject;
 import com.sorabh.grabfood.api_response_classes.otp_response.OTPResponse;
 import com.sorabh.grabfood.domain.model.post.OderPostModel;
 import com.sorabh.grabfood.domain.network_api.Result;
 import com.sorabh.grabfood.domain.usecase.GetOTPUseCase;
+import com.sorabh.grabfood.util.Constants;
+import com.sorabh.grabfood.util.Keys;
 import dagger.hilt.android.lifecycle.HiltViewModel;
-import kotlinx.coroutines.flow.StateFlow;
 import javax.inject.Inject;
 
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\r\u001a\u00020\u000e2\u0006\u0010\u000f\u001a\u00020\u0010R\u001a\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\t\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\f\u00a8\u0006\u0011"}, d2 = {"Lcom/sorabh/grabfood/ui/viewmodel/OtpViewModel;", "Landroidx/lifecycle/ViewModel;", "getOTPUseCase", "Lcom/sorabh/grabfood/domain/usecase/GetOTPUseCase;", "(Lcom/sorabh/grabfood/domain/usecase/GetOTPUseCase;)V", "_otpFlow", "Lkotlinx/coroutines/flow/MutableStateFlow;", "Lcom/sorabh/grabfood/domain/network_api/Result;", "Lcom/sorabh/grabfood/api_response_classes/otp_response/OTPResponse;", "otpFlow", "Lkotlinx/coroutines/flow/StateFlow;", "getOtpFlow", "()Lkotlinx/coroutines/flow/StateFlow;", "getOtp", "", "postModel", "Lcom/sorabh/grabfood/domain/model/post/OderPostModel;", "app_debug"})
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000>\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\t\b\u0007\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0010\u0010\u0015\u001a\u00020\u00162\u0006\u0010\u0017\u001a\u00020\u0018H\u0002J\u000e\u0010\u0019\u001a\u00020\u00162\u0006\u0010\u001a\u001a\u00020\nJ\u000e\u0010\u001b\u001a\u00020\u00162\u0006\u0010\u001c\u001a\u00020\nJ\u000e\u0010\u001d\u001a\u00020\u00162\u0006\u0010\u001e\u001a\u00020\nJ\u000e\u0010\u001f\u001a\u00020\u00162\u0006\u0010 \u001a\u00020\nR\u001a\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\t\u001a\b\u0012\u0004\u0012\u00020\n0\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\r\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u000e\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u0010R\u0017\u0010\u0011\u001a\b\u0012\u0004\u0012\u00020\n0\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\fR\u0017\u0010\u0013\u001a\b\u0012\u0004\u0012\u00020\n0\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\f\u00a8\u0006!"}, d2 = {"Lcom/sorabh/grabfood/ui/viewmodel/OtpViewModel;", "Landroidx/lifecycle/ViewModel;", "getOTPUseCase", "Lcom/sorabh/grabfood/domain/usecase/GetOTPUseCase;", "(Lcom/sorabh/grabfood/domain/usecase/GetOTPUseCase;)V", "_otpFlow", "Lkotlinx/coroutines/flow/MutableStateFlow;", "Lcom/sorabh/grabfood/domain/network_api/Result;", "Lcom/sorabh/grabfood/api_response_classes/otp_response/OTPResponse;", "confirmPasswordFlow", "", "getConfirmPasswordFlow", "()Lkotlinx/coroutines/flow/MutableStateFlow;", "otpFlow", "Lkotlinx/coroutines/flow/StateFlow;", "getOtpFlow", "()Lkotlinx/coroutines/flow/StateFlow;", "passwordFlow", "getPasswordFlow", "userOTPFlow", "getUserOTPFlow", "getOtp", "", "postModel", "Lcom/sorabh/grabfood/domain/model/post/OderPostModel;", "onConfirmPasswordChanged", "confirmPassword", "onPasswordChanged", "password", "onUserOTPChanged", "otp", "setupApiCall", "phone", "app_debug"})
 @dagger.hilt.android.lifecycle.HiltViewModel
 public final class OtpViewModel extends androidx.lifecycle.ViewModel {
     @org.jetbrains.annotations.NotNull
@@ -18,6 +20,12 @@ public final class OtpViewModel extends androidx.lifecycle.ViewModel {
     private final kotlinx.coroutines.flow.MutableStateFlow<com.sorabh.grabfood.domain.network_api.Result<com.sorabh.grabfood.api_response_classes.otp_response.OTPResponse>> _otpFlow = null;
     @org.jetbrains.annotations.NotNull
     private final kotlinx.coroutines.flow.StateFlow<com.sorabh.grabfood.domain.network_api.Result<com.sorabh.grabfood.api_response_classes.otp_response.OTPResponse>> otpFlow = null;
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> userOTPFlow = null;
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> passwordFlow = null;
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> confirmPasswordFlow = null;
     
     @javax.inject.Inject
     public OtpViewModel(@org.jetbrains.annotations.NotNull
@@ -30,7 +38,37 @@ public final class OtpViewModel extends androidx.lifecycle.ViewModel {
         return null;
     }
     
-    public final void getOtp(@org.jetbrains.annotations.NotNull
-    com.sorabh.grabfood.domain.model.post.OderPostModel postModel) {
+    @org.jetbrains.annotations.NotNull
+    public final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> getUserOTPFlow() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> getPasswordFlow() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> getConfirmPasswordFlow() {
+        return null;
+    }
+    
+    public final void onUserOTPChanged(@org.jetbrains.annotations.NotNull
+    java.lang.String otp) {
+    }
+    
+    public final void onPasswordChanged(@org.jetbrains.annotations.NotNull
+    java.lang.String password) {
+    }
+    
+    public final void onConfirmPasswordChanged(@org.jetbrains.annotations.NotNull
+    java.lang.String confirmPassword) {
+    }
+    
+    public final void setupApiCall(@org.jetbrains.annotations.NotNull
+    java.lang.String phone) {
+    }
+    
+    private final void getOtp(com.sorabh.grabfood.domain.model.post.OderPostModel postModel) {
     }
 }
