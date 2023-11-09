d49677a5e6d17e756a1e838d9b208417e23fb6fe
Sorabh Kumar <sorabh.kumar@stylework.city>
2023-11-09 09:06:33 +0530

    chore:migrated from kapt to ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins{
     id("com.android.application") version "8.1.2" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     kotlin("jvm") version "1.9.20"  apply false
     kotlin("android") version "1.9.20"  apply false
 }
